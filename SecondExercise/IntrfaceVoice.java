4a58ea0f43b325ac9cd9f60dcca5660699df30c4
swnirk <54669541+swnirk@users.noreply.github.com>
2021-04-12 12:18:25 +0300

    Create IntrfaceVoice.java

---
+++ SecondExercise/IntrfaceVoice.java
@@ -0,0 +1,39 @@
+public interface Voice {
+
+    void voice();
+}
+    class Cat implements Voice {
+
+        @Override
+        public void voice() {
+            System.out.println("Meow!");
+        }
+    }
+
+    class Dog implements Voice {
+
+
+        @Override
+        public void voice() {
+            System.out.println("Wow!");
+        }
+    }
+
+    class Cow implements Voice {
+
+        @Override
+        public void voice() {
+            System.out.println("Myy!");
+        }
+    }
+
+class InterfaceRun {
+
+    public static void main (String[] args) {
+
+        Voice [] animals = {new Cat(), new Dog(), new Cow()};
+
+        for (Voice s: animals)
+            s.voice();
+    }
+}
